ebdc7b30117ac3da9230b480e332e9e074298789
thathsaraudari <105314224+thathsaraudari@users.noreply.github.com>
2025-04-17 18:52:30 +0200

    Iteration 1 | Tongue Twister completed

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = s1+ " "+ s2 + " " + s3 + " "+ s4 +" "+ s5 + " "+ s3 + " "+ s2 + " "+ s1 + " " + s4;
 
 // Print out the concatenated string
 
 
+console.log(tongueTwister);
 
 
 /*******************************************
